28b436cfa9a2214976e506de0a3bbe9dfae2bea7
Johan Dahl <Johan.Dahl@lth.lu.se>
2019-08-30 16:58:53 +0200

    Info box for right column

---
+++ LuInfobox.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="pimcore_area_snippet pimcore_area_content">
+    <div class="pimcore_area_infobox pimcore_area_content">
+      <div class="bg-sky-50 mb-6 p-5">
+        <p class="h3 mt-0 mb-3">{{title}}</p>
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'LuInfobox',
+    props: {
+      title: String
+    }
+  }
+</script>
