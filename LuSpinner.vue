f0b3275c958877601d73748b1cea5e533cb57ee3
Johan Dahl <Johan.Dahl@lth.lu.se>
2019-10-09 11:55:19 +0200

    LuSpinner hide/show not working for functional component. Bump version 1.0.7

--- LuSpinner.vue
+++ LuSpinner.vue
@@ -1,6 +1,15 @@
-<template functional>
+<template>
     <div class="spinner text-center">
       <p class="text-primary"><i class="fal fa-circle-notch fa-2x fa-spin"></i></p>
-      <p class="font-weight-bold">{{ props.text }}</p>
+      <p class="font-weight-bold">{{ text }}</p>
     </div>
 </template>
+
+<script>
+export default {
+  name: 'LuSpinner',
+  props: {
+    text: String
+  }
+}
+</script>
