ca23783f78189a64bb0935c0bcbcd6362ad6e82a
Johan Dahl <Johan.Dahl@lth.lu.se>
2021-10-19 15:47:44 +0200

    v.1.8.6 Fix active on LuTopMenuItem
    * link get add class active by vue-router. But not enclosing item

--- LuTopMenuItem.vue
+++ LuTopMenuItem.vue
@@ -1,24 +1,24 @@
 <template>
-  <li v-if="item.children" class="nav-item dropdown dropdown-hover ">
+  <li v-if="item.children" class="nav-item dropdown dropdown-hover " :class="{active: active}">
     <router-link v-if="item.path" id="dropdown-studera" :to="item.path" class="nav-link text-nowrap dropdown-toggle "
                  aria-haspopup="true"
     >
       {{ $t(item.label) }}
     </router-link>
-    <a v-else id="dropdown-studera" :href="item.url" class="nav-link text-nowrap dropdown-toggle "
+    <a v-else :href="item.url" class="nav-link text-nowrap dropdown-toggle "
        aria-haspopup="true"
     >{{ $t(item.label) }}</a>
     <div class="dropdown-menu font-size-base" :class="[lastItem ? 'dropdown-menu-right' : '']" aria-labelledby="dropdown-studera">
       <lu-dropdown-item v-for="subMenuItem in item.children" :key="subMenuItem.id" :item="subMenuItem" />
     </div>
   </li>
-  <li v-else class="nav-item">
-    <router-link v-if="item.path" id="dropdown-start" :to="item.path" class="nav-link text-nowrap"
+  <li v-else class="nav-item" :class="{active: active}">
+    <router-link v-if=" item.path" :to="item.path" class="nav-link text-nowrap"
                  aria-haspopup="true"
     >
       {{ $t(item.label) }}
     </router-link>
-    <a v-else id="dropdown-start" :href="item.url" class="nav-link text-nowrap" aria-haspopup="true">{{ $t(item.label) }}</a>
+    <a v-else :href="item.url" class="nav-link text-nowrap" aria-haspopup="true">{{ $t(item.label) }}</a>
   </li>
 </template>
 
@@ -34,9 +34,14 @@ export default {
     item: { type: Object, required: true },
     lastItem: Boolean
   },
-  data () {
-    return {
+  computed: {
+    active () {
+      console.log(this.$route)
+      return this.$route.path === this.item.path
     }
+  },
+  mounted () {
+    console.log(this.$route)
   }
 }
 </script>
